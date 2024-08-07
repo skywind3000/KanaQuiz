73cdcbf8690ec98ec80799e14d7c1ed032d36965
skywind3000 <skywind3000@163.com>
2024-08-07 16:30:19 +0800

    fix: transparent background

--- kanaquiz.py
+++ kanaquiz.py
@@ -276,7 +276,10 @@ class configure (object):
                 foreground = color & 7
                 background = (color >> 4) & 7
                 bold = color & 8
-                sys.stdout.write("\033[%s3%d;4%dm"%(bold and "01;" or "", foreground, background))
+                if background != 0:
+                    sys.stdout.write("\033[%s3%d;4%dm"%(bold and "01;" or "", foreground, background))
+                else:
+                    sys.stdout.write("\033[%s3%dm"%(bold and "01;" or "", foreground))
                 sys.stdout.flush()
             else:
                 sys.stdout.write("\033[0m")
